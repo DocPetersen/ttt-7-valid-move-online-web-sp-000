799712bef960b5d5d4391314fb85092acf67302d
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-10-04 01:40:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,8 +8,8 @@ end
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
-    return true
-  if board[index] == "X" || board[index] == "O"
     return false
+  if board[index] == "X" || board[index] == "O"
+    return true
   end
 end
